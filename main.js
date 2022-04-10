3a678366d0f529dab2a2e3d37bb93c87b8c77e55
Siddharth Singh <simsid66@gmail.com>
2022-04-10 07:45:28 +0530

    first working version

---
+++ main.js
@@ -0,0 +1,38 @@
+import domtoimage from 'https://esm.sh/dom-to-image';
+
+const render = (text, div) => {
+    const lines = text.split("\n");
+    div.innerHTML = '';
+    for (const line of lines) {
+        const node = document.createElement('span');
+        node.textContent = line;
+        div.appendChild(node);
+    }
+}
+
+const download = (blob, filename) => {
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(blob);
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+    const textarea = document.querySelector('#input>textarea');
+    const outputContainer = document.querySelector('#output');
+    const output = document.querySelector("#output-lines");
+    const fgColor = document.querySelector('#output-fg-color');
+    const bgColor = document.querySelector('#output-bg-color');
+    const opFont = document.querySelector('#output-font');
+    const dl = document.querySelector('#download-btn');
+
+    textarea.oninput = () => render(textarea.value, output);
+    fgColor.oninput = () => outputContainer.style.color = fgColor.value;
+    bgColor.oninput = () => outputContainer.style.backgroundColor = bgColor.value;
+    opFont.oninput = () => outputContainer.style.fontFamily = opFont.value;
+    dl.onclick = () => domtoimage.toBlob(outputContainer).then((data) => {
+        download(data, 'poem.png');
+    })
+})
